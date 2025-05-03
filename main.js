49296c7af1a3b69b0e72d7b37efdcccd7ebd0b22
StraightforwardDigital <111469465+StraightforwardDigital@users.noreply.github.com>
2025-05-03 10:16:16 -0300

    adding js and sketch

---
+++ main.js
@@ -0,0 +1,7 @@
+// function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+//   // Return the solution to the addition of the three variables
+//   return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+// }
+
+// Return the solution to the addition of the three variables
+goals = (laLigaGoals, copaDelReyGoals, championsLeagueGoals) => laLigaGoals + copaDelReyGoals + championsLeagueGoals;
